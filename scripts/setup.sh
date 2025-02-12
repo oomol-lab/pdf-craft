c8aba15acebbd63138c4ae699e30dbea4e603c78
TaoZeyu <i@taozeyu.com>
2025-02-12 14:22:58 +0800

    feat(project): setup env

---
+++ scripts/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/.."
+
+if ! command -v conda >/dev/null 2>&1; then
+  echo "Need Conda to setup the environment"
+  exit 1
+fi
+
+if [ -d ".venv" ]; then
+  rm -rf .venv
+fi
+
+conda create --prefix ./.venv python=3.12.7 -y
+
+eval "$(conda shell.bash hook)"
+conda activate ./.venv
+
+pip install --upgrade pip
+pip install -r requirements.txt
---
+++ scripts/sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR/.."
+
+rm -rf ./doc_page_extractor
+cp -r ../../moskize91/doc_page_extractor/doc_page_extractor ./doc_page_extractor
