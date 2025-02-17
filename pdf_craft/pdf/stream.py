f6888c3c672d954801def09f3cacd2c92e0d75d2
TaoZeyu <i@taozeyu.com>
2025-02-17 15:22:53 +0800

    feat(pdf): add test codes for fitz

--- pdf_craft/pdf/stream.py
+++ pdf_craft/pdf/stream.py
@@ -1,12 +1,15 @@
 import os
 import io
+import fitz
 
 from dataclasses import dataclass
 from enum import Enum
 from typing import Iterable, Generator
-from PIL.Image import Image
+from PIL import ImageDraw
+from PIL.Image import frombytes, Image
 from pdfplumber import open
-from doc_page_extractor import plot, DocExtractor, ExtractedResult
+from pdfplumber.page import Page
+from doc_page_extractor import plot, Rectangle, DocExtractor, PreOCRFragment, ExtractedResult
 from .extractor import (
   extract,
   ExtractedTitle,
@@ -83,10 +86,60 @@ def _extract_page_result(doc_extractor: DocExtractor, pdf_file: str, debug_outpu
     for i, page in enumerate(pdf.pages):
       dpi = 300 # for scanned book pages
       image = page.to_image(resolution=dpi).annotated
-      result = doc_extractor.extract(image, "ch")
+      result = doc_extractor.extract(
+        image=image,
+        lang="ch",
+        adjust_rotation=False,
+        adjust_points=False,
+        pre_fragments=list(_extract_pre_ocr_fragments(i, image, page)),
+      )
       if debug_output is not None:
         _generate_plot(image, i, result, debug_output)
       yield result
+
+def _extract_pre_ocr_fragments(i: int, image: Image, page: Page) -> Generator[PreOCRFragment, None, None]:
+  image_width = float(image.size[0])
+  image_height = float(image.size[1])
+
+  with fitz.open("/Users/taozeyu/Downloads/中国古代练丹家的目的.pdf") as doc:
+    p = doc.load_page(i)
+    mat = fitz.Matrix(300 / 72, 300 / 72)
+    pixmap = p.get_pixmap(matrix=mat)
+    image_p = frombytes("RGB", [pixmap.width, pixmap.height], pixmap.samples)
+    draw = ImageDraw.Draw(image_p, mode="RGBA")
+    page_width = p.rect.width
+    page_height = p.rect.height
+
+    def handle_x(x: float) -> float:
+      return (x / page_width) * image_width
+
+    def handle_y(y: float) -> float:
+      return (y / page_height) * image_height
+
+    for e in p.get_text("words"):
+      x0, y0, x1, y1, text, _1, _2, _3 = e
+      x0: float = handle_x(e[0])
+      y0: float = handle_y(e[1])
+      x1: float = handle_x(e[2])
+      y1: float = handle_y(e[3])
+
+  # for word in page.extract_words():
+    # x0: float = handle_x(word["x0"])
+    # y0: float = handle_y(word["top"])
+    # x1: float = handle_x(word["x1"])
+    # y1: float = handle_y(word["bottom"])
+      react = Rectangle(
+        lt=(x0, y0),
+        rt=(x1, y0),
+        lb=(x0, y1),
+        rb=(x1, y1),
+      )
+      draw.polygon([p for p in react], outline=(255,0,0), width=3)
+      yield PreOCRFragment(
+        text=text,
+        rect=react,
+      )
+      image_p.save(f"/Users/taozeyu/codes/github.com/oomol-lab/pdf-craft/output/test_{i}.png")
 
 def _text(texts: Iterable[str]) -> str:
   buffer = io.StringIO()
