64745cce353803121f2e6d08b151c79bdc488632
TaoZeyu <i@taozeyu.com>
2025-02-18 10:32:27 +0800

    feat(main): write to markdown file

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from pdf_craft.pdf import stream_pdf, Text, TextKind
 from doc_page_extractor import DocExtractor
 
 def main():
-  pdf_file = "/Users/taozeyu/Downloads/临炉前的准备⼯作.pdf"
+  pdf_file = "/Users/taozeyu/Downloads/长觉能测试.pdf"
   model_dir_path = _project_dir_path("models")
   output_dir_path = _project_dir_path("output", True)
+  markdown_path = os.path.join("output", "output.md")
   extractor = DocExtractor(model_dir_path, order_by_layoutreader=False)
-  lines: list[str] = []
 
-  for item in stream_pdf(extractor, pdf_file, output_dir_path):
-    if not isinstance(item, Text):
-      continue
-    if item.kind == TextKind.TITLE:
-      lines.append("# " + item.text)
-    elif item.kind == TextKind.PLAIN_TEXT:
-      lines.append(item.text)
-
-  for line in lines:
-    print(line)
-    print("")
+  with open(markdown_path, "w", encoding="utf-8") as file:
+    for item in stream_pdf(extractor, pdf_file, output_dir_path):
+      if not isinstance(item, Text):
+        continue
+      if item.kind == TextKind.TITLE:
+        file.write("# " + item.text)
+        file.write("\n\n")
+      elif item.kind == TextKind.PLAIN_TEXT:
+        file.write(item.text)
+        file.write("\n\n")
+      file.flush()
 
 def _project_dir_path(name: str, clean: bool = False) -> str:
   path = os.path.join(__file__, "..", name)
