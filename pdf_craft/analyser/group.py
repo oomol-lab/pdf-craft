f3aee5ab3f2e7e23cf1a6e0cac944d8ee19327f0
TaoZeyu <i@taozeyu.com>
2025-02-22 13:29:33 +0800

    feat(analyser): complete report logic

--- pdf_craft/analyser/group.py
+++ pdf_craft/analyser/group.py
@@ -11,21 +11,28 @@ _Item = TextInfo | Segment
 
 @dataclass
 class Group:
-  pre_count: int
-  next_count: int
-  items: list[_Item]
+  head_remain_tokens: int
+  tail_remain_tokens: int
+  head: list[TextInfo | Segment]
+  body: list[TextInfo | Segment]
+  tail: list[TextInfo | Segment]
+
+@dataclass
+class _Attributes:
+  max_tokens: int
+  gap_max_tokens: float
+  tail_rate: float
 
 class _Group:
-  def __init__(self, max_tokens: int, gap_max_tokens: float):
-    self._max_tokens: int = max_tokens
-    self._gap_max_tokens: int = gap_max_tokens
-    body_max_tokens = max_tokens - gap_max_tokens * 2
+  def __init__(self, attr: _Attributes):
+    self._attr: _Attributes = attr
+    body_max_tokens = attr.max_tokens - attr.gap_max_tokens * 2
     assert body_max_tokens > 0
 
     # head and tail are passed to LLM as additional text
     # to let LLM understand the context of the body text.
-    self.head: _Buffer = _Buffer(gap_max_tokens)
-    self.tail: _Buffer = _Buffer(gap_max_tokens)
+    self.head: _Buffer = _Buffer(attr.gap_max_tokens)
+    self.tail: _Buffer = _Buffer(attr.gap_max_tokens)
     self.body: _Buffer = _Buffer(body_max_tokens)
 
   def append(self, item: _Item) -> bool:
@@ -42,7 +49,7 @@ class _Group:
     return success
 
   def next(self) -> _Group:
-    next_group: _Group = _Group(self._max_tokens, self._gap_max_tokens)
+    next_group: _Group = _Group(self._attr)
     next_head = next_group.head
     for item in reversed([*self.head, *self.body]):
       if next_head.can_append(item):
@@ -53,7 +60,43 @@ class _Group:
     return next_group
 
   def report(self) -> Group:
-    raise NotImplementedError("TODO:")
+    tokens: int = 0
+    for buffer in (self.head, self.body, self.tail):
+      tokens += buffer._tokens
+
+    head_remain_tokens = self.head.tokens
+    tail_remain_tokens = self.tail.tokens
+
+    if tokens > self._attr.max_tokens:
+      if self.body.tokens > self._max_tokens:
+        head_remain_tokens = 0
+        tail_remain_tokens = 0
+      else:
+        tail_rate = self._attr.tail_rate
+        remain_tokens = self._attr.max_tokens - self.body.tokens
+        if self.head.tokens < remain_tokens * (1.0 - tail_rate):
+          tail_remain_tokens = remain_tokens - self.head.tokens
+        elif self.tail.tokens < remain_tokens * tail_rate:
+          head_remain_tokens = remain_tokens - self.tail.tokens
+        else:
+          head_remain_tokens = round(remain_tokens * (1.0 - tail_rate))
+          tail_remain_tokens = round(remain_tokens * tail_rate)
+
+    head = self.head._items
+    tail = self.tail._items
+
+    if head_remain_tokens == 0:
+      head = []
+    if tail_remain_tokens == 0:
+      tail = []
+
+    return Group(
+      head_remain_tokens=head_remain_tokens,
+      tail_remain_tokens=tail_remain_tokens,
+      head=head,
+      body=self.body._items,
+      tail=tail,
+    )
 
 class _Buffer:
   def __init__(self, max_tokens: int):
@@ -69,6 +112,10 @@ class _Buffer:
   @property
   def has_any(self) -> bool:
     return len(self._items) > 0
+
+  @property
+  def tokens(self) -> int:
+    return self._tokens
 
   def seal(self):
     self._is_sealed = True
@@ -92,7 +139,12 @@ class _Buffer:
     next_tokens = self._tokens + item.tokens
     return next_tokens <= self._max_tokens
 
-def group(items: Iterable[TextInfo | Segment], max_tokens: int, gap_rate: float):
+def group(
+    items: Iterable[TextInfo | Segment],
+    max_tokens: int,
+    gap_rate: float,
+    tail_rate: float,
+  ):
   curr_group: _Group | None = None
   stream: Stream[_Item] = Stream(items)
   while True:
@@ -102,7 +154,8 @@ def group(items: Iterable[TextInfo | Segment], max_tokens: int, gap_rate: float)
     if curr_group is None:
       gap_max_tokens = floor(max_tokens * gap_rate)
       assert gap_max_tokens > 0
-      curr_group = _Group(max_tokens, gap_max_tokens)
+      attr = _Attributes(max_tokens, gap_max_tokens, tail_rate)
+      curr_group = _Group(attr)
       curr_group.head.seal()
 
     success = curr_group.append(item)
