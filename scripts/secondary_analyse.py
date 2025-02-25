320884b402cd7c3abc0b97e20a7aed62aa25adab
Moskize91 <i@taozeyu.com>
2025-02-25 17:38:26 +0800

    feat(analyser): support extracting index infomations (#18)

--- scripts/secondary_analyse.py
+++ scripts/secondary_analyse.py
@@ -12,7 +12,7 @@ from pdf_craft.analyser.secondary import SecondaryAnalyser
 def main():
   dir_path = os.path.join(__file__, "..", "..", "output")
   dir_path = os.path.abspath(dir_path)
-  # dir_path = "/Users/taozeyu/Downloads/并非旨在使人正常化的分析"
+  # dir_path = "/Users/taozeyu/Downloads/人类的由来与性选择目录"
   analyser = SecondaryAnalyser(
     llm=LLM(**_read_format_json()),
     dir_path=dir_path,
--- scripts/structure_pdf.py
+++ scripts/structure_pdf.py
@@ -7,14 +7,16 @@ import shutil
 sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
 
 from tqdm import tqdm
-from pdf_craft import PDFPageExtractor
-from pdf_craft.analyser.page_structure import structure
+from typing import Generator
+from pdf_craft import PDFPageExtractor, Block
+from pdf_craft.analyser.preliminary import preliminary_analyse
 from pdf_craft.analyser.llm import LLM
 
 
 def main():
-  pdf_file = os.path.join(__file__, "..", "..", "tests", "assets", "citation.pdf")
+  pdf_file = os.path.join(__file__, "..", "..", "tests", "assets", "index.pdf")
   pdf_file = os.path.abspath(pdf_file)
+  pdf_file = "/Users/taozeyu/Downloads/人类的由来与性选择目录.pdf"
   model_dir_path = _project_dir_path("models")
   output_dir_path = _project_dir_path("output", clean=True)
   page_dir_path = os.path.join(output_dir_path, "pages")
@@ -29,18 +31,20 @@ def main():
     model_dir_path=model_dir_path,
     debug_dir_path=os.path.join("output", "plot"),
   )
+  preliminary_analyse(
+    llm=llm,
+    page_dir_path=page_dir_path,
+    assets_dir_path=assets_dir_path,
+    blocks_matrix=_extract_blocks(pdf_file, extractor),
+  )
+
+def _extract_blocks(pdf_file: str, extractor: PDFPageExtractor) -> Generator[list[Block], None, None]:
   with fitz.open(pdf_file) as pdf:
-    for i, blocks in enumerate(tqdm(
+    for blocks in tqdm(
       iterable=extractor.extract(pdf, "ch"),
       total=pdf.page_count,
-    )):
-      output_file_path = os.path.join(page_dir_path, f"page_{i+1}.xml")
-      structure(
-        llm,
-        blocks,
-        output_file_path,
-        assets_dir_path,
-      )
+    ):
+      yield blocks
 
 def _project_dir_path(name: str, clean: bool = False) -> str:
   path = os.path.join(__file__, "..", "..", name)
