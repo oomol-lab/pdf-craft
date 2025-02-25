334f5f02e9f06f2edaa58cd4224efdac7513be42
TaoZeyu <i@taozeyu.com>
2025-02-25 11:10:48 +0800

    feat(analyser): support quote

--- scripts/secondary_analyse.py
+++ scripts/secondary_analyse.py
@@ -10,9 +10,12 @@ from pdf_craft.analyser.secondary import SecondaryAnalyser
 
 
 def main():
+  dir_path = os.path.join(__file__, "..", "..", "output")
+  dir_path = os.path.abspath(dir_path)
+  # dir_path = "/Users/taozeyu/Downloads/并非旨在使人正常化的分析"
   analyser = SecondaryAnalyser(
     llm=LLM(**_read_format_json()),
-    dir_path="/Users/taozeyu/Downloads/并非旨在使人正常化的分析",
+    dir_path=dir_path,
   )
   output_dir_path = os.path.join(__file__, "..", "..", "output", "citations")
   output_dir_path = os.path.abspath(output_dir_path)
