6298b1f58b65ab64e8d6d26a2155375570ac143a
TaoZeyu <i@taozeyu.com>
2025-02-26 14:19:45 +0800

    feat(project): prepare test PDF

--- scripts/secondary_chapters.py
+++ scripts/secondary_chapters.py
@@ -11,7 +11,7 @@ from pdf_craft.analyser.secondary import SecondaryAnalyser
 def main():
   dir_path = os.path.join(__file__, "..", "..", "output")
   dir_path = os.path.abspath(dir_path)
-  # dir_path = "/Users/taozeyu/Downloads/并非旨在使人正常化的分析"
+  # dir_path = "/Users/taozeyu/Downloads/中国古代农业"
   analyser = SecondaryAnalyser(
     llm=LLM(**_read_format_json()),
     dir_path=dir_path,
