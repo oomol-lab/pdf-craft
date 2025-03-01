e257d2b0b6c3da60d6e3cdde97da3f31a1c9c096
TaoZeyu <i@taozeyu.com>
2025-03-01 16:43:17 +0800

    feat(analyser): add deduplication logic

--- pdf_craft/analyser/serial.py
+++ pdf_craft/analyser/serial.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 import os
+import io
+import re
 
 from dataclasses import dataclass
 from typing import Iterable, Generator
@@ -11,10 +13,13 @@ from .asset_matcher import ASSET_TAGS
 from .utils import read_files, search_xml_children, parse_page_indexes
 
 
+def serials(llm: LLM, index: Index | None, chunks_path: str) -> Generator[Serial, None, None]:
+  yield from _Deduplication(llm, index, chunks_path).for_serials()
+
 @dataclass
 class Serial:
   main_texts: list[Element]
-  citations: dict[int, Citation]
+  citations: Citations
 
 @dataclass
 class Citation:
@@ -22,8 +27,27 @@ class Citation:
   label: str
   text: Element
 
-def serials(llm: LLM, index: Index | None, chunks_path: str) -> Generator[Serial, None, None]:
-  yield from _Deduplication(llm, index, chunks_path).for_serials()
+class Citations:
+  def __init__(self):
+    self._refs: dict[int, tuple[int, Citation]] = {}
+
+  def ref(self, citation: Citation) -> Citation:
+    id = citation.id
+    if id in self._refs:
+      count, citation = self._refs[id]
+      self._refs[id] = (count + 1, citation)
+    else:
+      self._refs[id] = (1, citation)
+    return citation
+
+  def unref(self, id: int) -> Citation:
+    assert id in self._refs, f"Cannot find citation with id {id}"
+    count, citation = self._refs[id]
+    if count == 1:
+      self._refs.pop(id)
+    else:
+      self._refs[id] = (count - 1, citation)
+    return citation
 
 @dataclass
 class _Chunk:
@@ -31,7 +55,7 @@ class _Chunk:
   start_idx: int
   end_idx: int
   index: int
-  buffer: Element | None
+  serial: Serial | None
 
 class _Deduplication:
   def __init__(self, llm: LLM, index: Index | None, chunks_path: str):
@@ -57,57 +81,39 @@ class _Deduplication:
 
   def for_serials(self) -> Generator[Serial, None, None]:
     for index, chunk in enumerate(self._chunks):
-      serial = self._load_serial(index, chunk)
-      chunk.buffer = None
+      serial = self._load_serial_and_deduplicate(index, chunk)
+      chunk.serial = None
       if serial is not None:
         yield serial
 
-  def _load_serial(self, index: int, chunk: _Chunk):
-    chunk_xml = self._load_xml(chunk)
-    content_xml = chunk_xml.find("content")
-    latest_text = self._find_end_text(content_xml, False)
+  def _load_serial_and_deduplicate(self, index: int, chunk: _Chunk) -> Serial | None:
+    serial = self._load_serial(chunk)
+    latest_text = self._find_end_text(serial, False)
     if latest_text is not None:
-      duplicated_texts = self._take_duplicated_texts(latest_text, index)
-      if len(duplicated_texts) > 0:
-        duplicated_texts.insert(0, latest_text)
-        merged_text = self._merge_texts(duplicated_texts)
-        if latest_text != merged_text:
-          self._replace_child(content_xml, latest_text, merged_text)
-          # TODO: 并非替换这么简单，citations 要怎么办？也要映射到正确的位置。
+      duplicated = list(self._find_duplicated_texts_from_serials(latest_text, index))
+      if len(duplicated) > 0:
+        latest_text_index = serial.main_texts.index(latest_text)
+        duplicated.insert(0, (latest_text, serial))
+        merged_text, citations = self._remove_and_merge_texts_from_serials(duplicated)
+        serial.main_texts.insert(latest_text_index, merged_text)
+        # TODO: 将 citations 重新插入
 
-    main_texts: list[Element] = []
-    for child in content_xml:
-      main_texts.append(child)
-
-    if len(main_texts) == 0:
+    if len(serial.main_texts) == 0:
       # cleared due to deduplication
-      return
+      return None
 
-    citations_xml = chunk_xml.find("citations")
-    citations: dict[int, Citation] = {}
+    return serial
 
-    if citations_xml is not None:
-      id = int(citations_xml.get("id"))
-      label = citations_xml.find("label").text
-      citations[id] = Citation(
-        id=id,
-        label=label,
-        text=citations_xml.find("text"),
-      )
-
-    return Serial(main_texts, citations)
-
-  def _take_duplicated_texts(self, text: Element, index: int) -> list[Element]:
-    duplicated_texts: list[Element] = []
+  def _find_duplicated_texts_from_serials(self, text: Element, index: int):
+    ban_max_index = index - 1 # the processed index cannot be processed again
     search_indexes = [i for i in parse_page_indexes(text) if i != index]
     search_indexes.sort()
 
     while len(search_indexes) > 0:
       next_index = search_indexes.pop(0)
-      next_chunk_xml = self._load_xml(self._chunks[next_index])
-      next_content_xml = next_chunk_xml.find("content")
-      first_text = self._find_end_text(next_content_xml, True)
-      if first_text is not None:
+      serial = self._load_serial(self._chunks[next_index])
+      first_text = self._find_end_text(serial, True)
+      if first_text is None:
         # If the process breaks down, it means that LLM made an error in judgment
         # and the process must be interrupted (this will not happen under every thing is OK)
         break
@@ -118,32 +124,108 @@ class _Deduplication:
         # Something must have gone wrong. To be on the safe side, end this operation.
         break
 
-      did_update = False
-      index = next_index
-      duplicated_texts.append(first_text)
+      yield first_text, serial
 
+      origin_indexes_count = len(search_indexes)
       for next_index in next_indexes:
-        if next_index not in search_indexes:
-          search_indexes.append(next_index)
-          did_update = True
-      if did_update:
+        if next_index in search_indexes:
+          continue
+        if next_index <= ban_max_index:
+          continue
+        search_indexes.append(next_index)
+
+      if origin_indexes_count != len(search_indexes):
         search_indexes.sort()
 
-    return duplicated_texts
+      index = next_index
+      ban_max_index = max(index)
 
-  def _merge_texts(self, texts: list[Element]) -> Element:
-    # TODO: Implement this method
-    raise NotImplementedError()
+  def _remove_and_merge_texts_from_serials(self, duplicated: list[tuple[Element, Serial]]):
+    citation_matrix: list[dict[int, Citation]] = []
+    for text, serial in duplicated:
+      citations: dict[int, Citation] = {}
+      citation_matrix.append(citations)
+      for id in self._search_ref_ids_in_text(text):
+        citation = serial.citations.unref(id)
+        citations[id] = citation
 
-  def _find_end_text(self, content_xml: Element, is_begin_end: bool) -> Element | None:
+      index = serial.main_texts.index(text)
+      if index > 0:
+        serial.main_texts.pop(index)
+
+    index = self._try_to_choose_from_texts(e[0] for e in duplicated)
+    if index == -1:
+      raise NotImplementedError("TODO: use LLM to choose the best text index")
+
+    text, _ = duplicated[index]
+    citations = citation_matrix[index]
+
+    return text, citations
+
+  def _try_to_choose_from_texts(self, texts: Iterable[Element]) -> int:
+    str_texts: list[str] = []
+    for text in texts:
+      buffer = io.StringIO()
+      buffer.write(self._normalize_text(text.text))
+      for child in text:
+        buffer.write("<ref/>")
+        buffer.write(self._normalize_text(child.tail))
+      str_texts.append(buffer.getvalue())
+
+    no_sub_indexes: list[int] = []
+    for i, str_text1 in enumerate(str_texts):
+      not_sub = False
+      for j, str_text2 in enumerate(str_texts):
+        if i != j and str_text1 in str_text2:
+          not_sub = True
+          break
+      if not_sub:
+        no_sub_indexes.append(i)
+
+    if len(no_sub_indexes) != 1:
+      return -1
+    return no_sub_indexes[0]
+
+  def _load_serial(self, chunk: _Chunk):
+    if chunk.serial is not None:
+      return chunk.serial
+
+    file_path = os.path.join(self._chunks_path, chunk.file_name)
+    with open(file_path, "r", encoding="utf-8") as file:
+      chunk_xml = fromstring(file.read())
+
+    if self._index is not None:
+      content_xml = chunk_xml.find("content")
+      self._index.mark_ids_for_headlines(self._llm, content_xml)
+
+    main_texts: list[Element] = []
+    for child in content_xml:
+      main_texts.append(child)
+
+    citations_xml = chunk_xml.find("citations")
+    citations = Citations()
+
+    if citations_xml is not None:
+      id = int(citations_xml.get("id"))
+      label = citations_xml.find("label").text
+      citations.ref(id, Citation(
+        id=id,
+        label=label,
+        text=citations_xml.find("text"),
+      ))
+    return Serial(main_texts, citations)
+
+  def _find_end_text(self, serial: Serial, is_begin_end: bool) -> Element | None:
+    main_texts = serial.main_texts
     range_iter: Iterable[int]
+
     if is_begin_end:
-      range_iter = range(len(content_xml))
+      range_iter = range(len(main_texts))
     else:
-      range_iter = range(len(content_xml) - 1, -1, -1)
+      range_iter = range(len(main_texts) - 1, -1, -1)
 
     for i in range_iter:
-      element = content_xml[i]
+      element = main_texts[i]
       if element.tag == "text":
         return element
       elif element.tag not in ASSET_TAGS:
@@ -152,27 +234,14 @@ class _Deduplication:
 
     return None
 
-  def _load_xml(self, chunk: _Chunk):
-    if chunk.buffer is None:
-      file_path = os.path.join(self._chunks_path, chunk.file_name)
-      with open(file_path, "r", encoding="utf-8") as file:
-        chunk_xml = fromstring(file.read())
-      chunk.buffer = chunk_xml
-      if self._index is not None:
-        content_xml = chunk_xml.find("content")
-        self._index.mark_ids_for_headlines(self._llm, content_xml)
-    return chunk.buffer
+  def _search_ref_ids_in_text(self, text: Element):
+    for target in search_xml_children(text):
+      if target.tag == "ref":
+        yield int(target.get("id"))
 
   def _clean_all_idx_attr(self, element: Element):
     for target in search_xml_children(element):
       target.attrib.pop("idx", None)
 
-  def _replace_child(self, parent: Element, old_child: Element, new_child: Element):
-    index: int = -1
-    for i, child in enumerate(parent):
-      if child == old_child:
-        index = i
-        break
-    if index == -1:
-      parent.remove(old_child)
-      parent.insert(index, new_child)
+  def _normalize_text(self, headline: str) -> str:
+    return re.sub(r"\s+", " ", headline).strip()
