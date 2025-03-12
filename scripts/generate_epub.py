cd9ee5e395a99b7a76ac78c08f6c6e7bddbadb3f
TaoZeyu <i@taozeyu.com>
2025-03-12 11:38:23 +0800

    feat(epub): write files into zip file

--- scripts/generate_epub.py
+++ scripts/generate_epub.py
@@ -4,25 +4,18 @@ import shutil
 
 sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
 
-from pdf_craft.epub.gen_files import generate_files
+from pdf_craft.epub.gen_epub import generate_epub_file
 
 
 def main():
   pdf_file = os.path.join(__file__, "..", "..", "tests", "assets", "figure-caption.pdf")
   pdf_file = os.path.abspath(pdf_file)
   output_dir_path = _project_dir_path("output")
-  epub_dir_path = os.path.join(output_dir_path, "epub")
-  epub_file_path = os.path.join(output_dir_path, "final")
 
-  shutil.rmtree(epub_dir_path, ignore_errors=True)
-  os.makedirs(epub_dir_path, exist_ok=True)
-
-  generate_files(
+  generate_epub_file(
     from_dir_path=output_dir_path,
-    output_dir_path=epub_dir_path,
+    epub_file_path=os.path.join(output_dir_path, "final.epub"),
   )
-  shutil.make_archive(epub_file_path, "zip", epub_dir_path)
-  os.rename(epub_file_path + ".zip", epub_file_path + ".epub")
 
 def _project_dir_path(name: str, clean: bool = False) -> str:
   path = os.path.join(__file__, "..", "..", name)
