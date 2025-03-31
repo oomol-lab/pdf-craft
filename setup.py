fdaf2608cfff4181e4f68ee81eb2f04671e3c1cc
Moskize91 <i@taozeyu.com>
2025-03-31 18:29:14 +0800

    chore(project): upgrade to 0.0.15 (#105)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="pdf-craft",
-  version="0.0.14",
+  version="0.0.15",
   author="Tao Zeyu",
   author_email="i@taozeyu.com",
   url="https://github.com/oomol-lab/pdf-craft",
