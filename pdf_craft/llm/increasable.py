079b9299e65a54a4f226fcf713b959d5a26e8c05
taozeyu <i@taozeyu.com>
2025-04-06 10:05:23 +0800

    refactor(llm): rename to value_range

--- pdf_craft/llm/increasable.py
+++ pdf_craft/llm/increasable.py
@@ -1,21 +1,21 @@
 class Increaser:
-  def __init__(self, range: tuple[float, float] | None):
-    self._range: tuple[float, float] | None = range
-    self._current: float | None = range[0] if range is not None else None
+  def __init__(self, value_range: tuple[float, float] | None):
+    self._value_range: tuple[float, float] | None = value_range
+    self._current: float | None = value_range[0] if value_range is not None else None
 
   @property
   def current(self) -> float | None:
     return self._current
 
   def increase(self):
-    if self._range is None:
+    if self._value_range is None:
       return
-    _, end_value = self._range
+    _, end_value = self._value_range
     self._current = self._current + 0.5 * (end_value - self._current)
 
 class Increasable:
   def __init__(self, param: float | tuple[float, float] | None):
-    self._range: tuple[float, float] | None = None
+    self._value_range: tuple[float, float] | None = None
 
     if isinstance(param, int):
       param = float(param)
@@ -29,7 +29,7 @@ class Increasable:
         begin = float(begin)
       if isinstance(end, int):
         end = float(end)
-      self._range = (begin, end)
+      self._value_range = (begin, end)
 
   def context(self) -> Increaser:
-    return Increaser(self._range)
+    return Increaser(self._value_range)
