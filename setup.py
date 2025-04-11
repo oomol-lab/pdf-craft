8b3c6de3c177a6689f53b1fcc708d5df26b8a77f
TaoZeyu <i@taozeyu.com>
2025-04-11 11:50:13 +0800

    chore(project): upgrade to 0.0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="pdf-craft",
-  version="0.0.18",
+  version="0.0.19",
   author="Tao Zeyu",
   author_email="i@taozeyu.com",
   url="https://github.com/oomol-lab/pdf-craft",
