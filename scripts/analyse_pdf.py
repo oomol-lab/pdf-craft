aab35b9fc88960b3bf85fc593090390fc4b4654b
TaoZeyu <i@taozeyu.com>
2025-05-30 14:48:05 +0800

    feat(scripts): call new API

--- scripts/analyse_pdf.py
+++ scripts/analyse_pdf.py
@@ -6,29 +6,29 @@ import shutil
 sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
 
 from tqdm import tqdm
+from pathlib import Path
 from pdf_craft import analyse, LLM, OCRLevel, PDFPageExtractor, AnalysingStep
 
 
 def main():
-  pdf_file = os.path.join(__file__, "..", "..", "tests", "assets", "index.pdf")
-  pdf_file = os.path.abspath(pdf_file)
+  pdf_file = Path(__file__) / ".." / ".." / "tests" / "assets" / "citation_large.pdf"
+  pdf_file = pdf_file.resolve()
   model_dir_path = _project_dir_path("models")
   output_dir_path = _project_dir_path("output", clean=True)
   analysing_dir_path = _project_dir_path("analysing", clean=False)
   bar: tqdm | None = None
 
   try:
-    def report_step(step: AnalysingStep, count: int):
+    def report_step(step: AnalysingStep):
       nonlocal bar
-      if bar is not None:
-        bar.close()
+      bar = None
       print("Step:", step.name)
-      bar = tqdm(total=count)
 
-    def report_progress(completed_count: int):
+    def report_progress(completed_count: int, max_count: int | None):
       nonlocal bar
-      if bar is not None:
-        bar.update(completed_count)
+      if bar is None:
+        bar = tqdm(total=max_count)
+      bar.update(completed_count)
 
     analyse(
       llm=LLM(**_read_format_json()),
@@ -48,19 +48,19 @@ def main():
     if bar is not None:
       bar.close()
 
-def _project_dir_path(name: str, clean: bool = False) -> str:
-  path = os.path.join(__file__, "..", "..", name)
-  path = os.path.abspath(path)
-  if clean:
-    shutil.rmtree(path, ignore_errors=True)
-  os.makedirs(path, exist_ok=True)
-  return path
-
 def _read_format_json() -> dict:
-  path = os.path.join(__file__, "..", "..", "format.json")
-  path = os.path.abspath(path)
+  path = Path(__file__) / ".." / ".." / "format.json"
+  path = path.resolve()
   with open(path, mode="r", encoding="utf-8") as file:
     return json.load(file)
+
+def _project_dir_path(name: str, clean: bool = False) -> Path:
+  path = Path(__file__) / ".." / ".." / name
+  path = path.resolve()
+  if clean:
+    shutil.rmtree(path, ignore_errors=True)
+  path.mkdir(parents=True, exist_ok=True)
+  return path
 
 if __name__ == "__main__":
   main()
