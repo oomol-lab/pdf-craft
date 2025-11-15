78243389d26bcc968a935933132687dad2731ad7
TaoZeyu <i@taozeyu.com>
2025-11-15 12:56:02 +0800

    chore: upgrade to 1.0.0rc8

--- pdf_craft/__init__.py
+++ pdf_craft/__init__.py
@@ -2,5 +2,5 @@ from doc_page_extractor import AbortError, TokenLimitError
 from epub_generator import BookMeta, TableRender, LaTeXRender
 
 from .pdf import ocr_pdf, pdf_pages_count, predownload_models, DeepSeekOCRModel, OCREvent, OCREventKind
-from .transform import transform_epub, transform_markdown
+from .transform import transform_epub, transform_markdown, OCRTokensMetering
 from .aborted import AbortedCheck
