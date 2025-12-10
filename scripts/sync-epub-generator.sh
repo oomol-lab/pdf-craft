f933af5d1094960b5bb3c5e7079c69d091bf4e12
TaoZeyu <i@taozeyu.com>
2025-12-10 16:15:49 +0800

    chore: fix bug of scripts

--- scripts/sync-epub-generator.sh
+++ scripts/sync-epub-generator.sh
@@ -3,5 +3,5 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/.."
 
-rm -rf ./.venv/lib/python3.11/site-packages/epub-generator
+rm -rf ./.venv/lib/python3.11/site-packages/epub_generator
 cp -r ../epub-generator/epub_generator ./.venv/lib/python3.11/site-packages/epub_generator
