731c6d0b6531503b8687b2d4650daf294d4b9955
Moskize91 <i@taozeyu.com>
2025-12-20 12:52:20 +0800

    fix: override some of content (#312)

--- pdf_craft/common/xml.py
+++ pdf_craft/common/xml.py
@@ -1,28 +1,20 @@
 from pathlib import Path
-from typing import Container
 from xml.etree.ElementTree import tostring, fromstring, Element
 
 
-def indent(elem: Element, level: int = 0, skip_tags: Container[str] = ()) -> Element:
+def indent(elem: Element, level: int = 0) -> Element:
     indent_str = "  " * level
     next_indent_str = "  " * (level + 1)
-
-    if elem.tag in skip_tags:
-        if level > 0 and (not elem.tail or not elem.tail.strip()):
-            elem.tail = "\n" + indent_str
-        return elem
-
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = "\n" + next_indent_str
         for i, child in enumerate(elem):
-            indent(child, level + 1, skip_tags)
-            if i < len(elem) - 1:
-                child.tail = "\n" + next_indent_str
-            else:
-                child.tail = "\n" + indent_str
-    elif level > 0 and (not elem.tail or not elem.tail.strip()):
-        elem.tail = "\n" + indent_str
+            indent(child, level + 1)
+            if not child.tail or not child.tail.strip():
+                if i == len(elem) - 1:
+                    child.tail = "\n" + indent_str
+                else:
+                    child.tail = "\n" + next_indent_str
     return elem
 
 def read_xml(file_path: Path) -> Element:
